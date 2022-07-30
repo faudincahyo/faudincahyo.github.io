6264781041623742a4674a3dd0ec30d1b06f234e
faudincahyo <fingergreem6@gmail.com>
2022-07-30 19:02:27 +0700

    Update element

--- register.php
+++ register.php
@@ -1,15 +1,14 @@
 <?php 
     $username = $_POST['username'];
-    $email = $_POST['email'];
     $password = $_POST['password'];
 
     $conn = new mysqli ('localhost','root','','register');
     if($conn->connect_error){
         die('Connection failed : '.$conn->connect_error);
     }else{
-        if($stmt = $conn->prepare("insert into regis (username,email,password)
-        value(?,?,?)")){
-        $stmt->bind_param("sss",$username,$email,$password);
+        if($stmt = $conn->prepare("insert into regis (username,password)
+        value(?,?)")){
+        $stmt->bind_param("ss",$username,$password);
         $stmt->execute();
             echo "<script>alert('User berhasil ditambahkan'); window.location='index.html'</script>";
             
